9c0b0205f2de8f60af7f5a731e177a027693d149
Mark Triggs <mtriggs@nla.gov.au>
2010-09-28 14:00:27 +1000

    Diacritic filter changes: Don't assume Java 1.6.  Use horrible reflection to invoke the right class for the right JVM version.

--- common/java/au/gov/nla/util/DiacriticStripper.java
+++ common/java/au/gov/nla/util/DiacriticStripper.java
@@ -1,15 +1,53 @@
-// Stolen verbatim from Simon Jacob.
+// Originally stolen from Simon Jacob and modified to work under JDK1.5 as well
+// as JDK1.6
 package au.gov.nla.util;
 
 
 import java.util.*;
 import java.text.*;
+import java.lang.reflect.*;
 
 public class DiacriticStripper
 {
+    private String normalise (String s) throws Exception
+    {
+        try {
+            Class normalizer = Class.forName ("java.text.Normalizer");
+            Class normalizerForm = Class.forName ("java.text.Normalizer$Form");
+
+            Method normalize = normalizer.getMethod ("normalize",
+                                                     Class.forName ("java.lang.CharSequence"),
+                                                     normalizerForm);
+
+            Method getForm = normalizerForm.getMethod ("valueOf",
+                                                       Class.forName ("java.lang.String"));
+
+            return (String)normalize.invoke (null, s, getForm.invoke (null, "NFKD"));
+        } catch (ClassNotFoundException e) {
+            Class normalizer = Class.forName ("sun.text.Normalizer");
+            Method normalize = normalizer.getMethod ("normalize",
+                                                     Class.forName ("java.lang.String"),
+                                                     Class.forName ("sun.text.Normalizer$Mode"),
+                                                     Integer.TYPE);
+
+            return (String)normalize.invoke (null,
+                                             s,
+                                             normalizer.getDeclaredField ("DECOMP_COMPAT").get (normalizer),
+                                             0);
+        }
+    }
+
+
     public String fix (String s)
     {
-        String termText = Normalizer.normalize(s, Normalizer.Form.NFKD);
+        String termText = s;
+        try {
+            termText = normalise (s);
+        } catch (Exception e) {
+            System.out.println ("WARNING: Failed to strip diacritics: " + e);
+            e.printStackTrace ();
+            // well, we tried...
+        }
 
         StringBuilder sb = new StringBuilder(termText.length() + 10);
         char[] chars = termText.toCharArray();
@@ -20,43 +58,43 @@ public class DiacriticStripper
             }
 
             switch(ch) {
-            case '[': break;				   // delete characters
+            case '[': break;                               // delete characters
             case ']': break;
             case '\'': break;
             case '.': break;
             case ',': break;
             case '/': break;
             case '-': break;
-            case '\u00C6':	sb.append("AE"); break; // AE digraph
-            case '\u00E6':	sb.append("ae"); break;
-            case '\u0152':	sb.append("OE"); break; // OE digraph
-            case '\u0153':	sb.append("oe"); break;
-            case '\u00DE':	sb.append("TH"); break; // Icelandic Thorn
-            case '\u00FE':	sb.append("th"); break;
-            case '\u0110':	sb.append("D"); break;  // Crossed D
-            case '\u0111':	sb.append("d"); break;
-            case '\u00F0':	sb.append("D"); break;  // Eth
-            case '\u00D0':	sb.append("d"); break;
-            case '\u0130':	sb.append("I"); break;  // Turkish I
-            case '\u0131':	sb.append("i"); break;
-            case '\u0141':	sb.append("L"); break;  // Polish L
-            case '\u0142':	sb.append("l"); break;
+            case '\u00C6':      sb.append("AE"); break; // AE digraph
+            case '\u00E6':      sb.append("ae"); break;
+            case '\u0152':      sb.append("OE"); break; // OE digraph
+            case '\u0153':      sb.append("oe"); break;
+            case '\u00DE':      sb.append("TH"); break; // Icelandic Thorn
+            case '\u00FE':      sb.append("th"); break;
+            case '\u0110':      sb.append("D"); break;  // Crossed D
+            case '\u0111':      sb.append("d"); break;
+            case '\u00F0':      sb.append("D"); break;  // Eth
+            case '\u00D0':      sb.append("d"); break;
+            case '\u0130':      sb.append("I"); break;  // Turkish I
+            case '\u0131':      sb.append("i"); break;
+            case '\u0141':      sb.append("L"); break;  // Polish L
+            case '\u0142':      sb.append("l"); break;
                 /* these are performed by the comp decomp above
-                   case '\u2112':	sb.append("L"); break;  // Script L
-                   case '\u2113':	sb.append("l"); break;
-                   case '\u01A0':	sb.append("O"); break;  // Hooked O
-                   case '\u01A1':	sb.append("o"); break;
-                   case '\u01AF':	sb.append("U"); break;  // Hooked U
-                   case '\u01B0':	sb.append("u"); break;
+                   case '\u2112':       sb.append("L"); break;  // Script L
+                   case '\u2113':       sb.append("l"); break;
+                   case '\u01A0':       sb.append("O"); break;  // Hooked O
+                   case '\u01A1':       sb.append("o"); break;
+                   case '\u01AF':       sb.append("U"); break;  // Hooked U
+                   case '\u01B0':       sb.append("u"); break;
                 */
-            case '\u00D8':	sb.append("O"); break;  // Slashed O
-            case '\u00F8':	sb.append("o"); break;
-            case '\u0391':	sb.append("A"); break;  // Alpha
-            case '\u03B1':	sb.append("a"); break;
-            case '\u0392':	sb.append("B"); break;  // Beta
-            case '\u03B2':	sb.append("b"); break;
-            case '\u0393':	sb.append("G"); break;  // Gamma
-            case '\u03B3':	sb.append("g"); break;
+            case '\u00D8':      sb.append("O"); break;  // Slashed O
+            case '\u00F8':      sb.append("o"); break;
+            case '\u0391':      sb.append("A"); break;  // Alpha
+            case '\u03B1':      sb.append("a"); break;
+            case '\u0392':      sb.append("B"); break;  // Beta
+            case '\u03B2':      sb.append("b"); break;
+            case '\u0393':      sb.append("G"); break;  // Gamma
+            case '\u03B3':      sb.append("g"); break;
             default: sb.append(ch);
             }
         }
