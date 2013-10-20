9fcf803e19512c140aa9eaf83d80ec3bdb609696
Mark Triggs <mark@dishevelled.net>
2013-10-21 09:47:59 +1100

    Add a junit4 test suite

---
+++ tests/org/vufind/solr/browse/tests/NormaliserTest.java
@@ -0,0 +1,16 @@
+package org.vufind.solr.browse.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+
+public class NormaliserTest
+{
+    @Test
+    public void thisAlwaysPasses () {
+        assertEquals ("hello", "hello");
+    }
+}
