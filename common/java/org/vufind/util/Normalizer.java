979c81b89507bd0f3d40b0fc5ee87bdabe110a22
Demian Katz <demian.katz@villanova.edu>
2014-10-28 11:33:28 -0400

    More comments.

--- common/java/org/vufind/util/Normalizer.java
+++ common/java/org/vufind/util/Normalizer.java
@@ -13,7 +13,11 @@ public interface Normalizer {
 	 * Normalizes the input string to a collation key, which will be used for the 
 	 * sort order of the browse index.
 	 * 
-	 * @param s	the string to be normalized, e.g. a term for the browse index
+	 * We use a byte array for the sort key instead of a string to ensure consistent
+	 * sorting even if the index tool and browse handler are running with different
+	 * locale settings. Using strings results in less predictable behavior.
+	 *
+	 * @param s the string to be normalized, e.g. a term for the browse index
 	 * @return	collation key for determining browse order
 	 */
 	public byte[] normalize (String s);
